3ffa5b458595a37547000bf23669bd4a7e0e42d0
kjh311 <kjh311@gmail.com>
2026-05-11 08:31:05 -0600

    chore: initial secure setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,3 +41,7 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("androidx.core:core-splashscreen:1.0.1")
+}
--- android/app/src/main/kotlin/com/jh311/route_alerts/MainActivity.kt
+++ android/app/src/main/kotlin/com/jh311/route_alerts/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.jh311.haul_alerts
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition before calling super.onCreate()
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+    }
+}
